139b2973bca040f1e8e15191f5738461fb3fd2a5
EES2001 <eecheverria499@unab.edu.co>
2024-08-28 09:51:46 -0500

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,23 +1,75 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const socketIo = require('socket.io');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIo(server);
+
+let teamsData = {
+    redTeam: [],
+    blueTeam: []
+};
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
 
-// Rutas API
-app.use('/api', require('./routes/api'));
+io.on('connection', (socket) => {
+    console.log('Nuevo usuario conectado');
 
-// Configurar Socket.IO
-require('./socket/io')(io);
+    socket.on('joinTeam', (data) => {
+        if (data.team === 'red') {
+            teamsData.redTeam.push({ id: socket.id, time: null });
+        } else {
+            teamsData.blueTeam.push({ id: socket.id, time: null });
+        }
+        saveData();
+    });
+
+    socket.on('startGame', () => {
+        const randomNumber = Math.floor(Math.random() * 10) + 1;
+        io.emit('startCountdown', randomNumber);
+    });
+
+    socket.on('userResponse', (data) => {
+        const teamArray = data.team === 'red' ? teamsData.redTeam : teamsData.blueTeam;
+        const user = teamArray.find(user => user.id === socket.id);
+        if (user) {
+            user.time = data.time;
+        }
+        saveData();
+        checkCompletion();
+    });
+
+    socket.on('resetGame', () => {
+        teamsData.redTeam = [];
+        teamsData.blueTeam = [];
+        io.emit('resetGame');
+        saveData();
+    });
+
+    socket.on('disconnect', () => {
+        teamsData.redTeam = teamsData.redTeam.filter(user => user.id !== socket.id);
+        teamsData.blueTeam = teamsData.blueTeam.filter(user => user.id !== socket.id);
+        saveData();
+    });
+});
+
+function saveData() {
+    fs.writeFileSync(path.join(__dirname, 'data', 'teams.json'), JSON.stringify(teamsData));
+}
+
+function checkCompletion() {
+    if (teamsData.redTeam.every(user => user.time !== null) && 
+        teamsData.blueTeam.every(user => user.time !== null)) {
+
+        const redAvg = teamsData.redTeam.reduce((acc, curr) => acc + curr.time, 0) / teamsData.redTeam.length;
+        const blueAvg = teamsData.blueTeam.reduce((acc, curr) => acc + curr.time, 0) / teamsData.blueTeam.length;
+        
+        io.emit('gameResult', { redAvg, blueAvg });
+    }
+}
 
 const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Servidor escuchando en puerto ${PORT}`));
